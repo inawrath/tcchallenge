a3254e7c5a643fe40cb65bbda0e6d6b8d0b8a8e7
Ivan Nawrath <inawrath@informaticos.com>
2021-02-06 22:35:57 -0300

    configure celery

--- tcchallenge/settings.py
+++ tcchallenge/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'django_celery_beat',
+    'core',
 ]
 
 MIDDLEWARE = [

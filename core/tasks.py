4f0ba5752c65f05ffd3e69a08e7b15ea3847c88e
Ivan Nawrath <inawrath@informaticos.com>
2021-02-08 00:06:56 -0300

    catch exception to task

--- core/tasks.py
+++ core/tasks.py
@@ -1,5 +1,8 @@
 from celery import shared_task
+from django.db import transaction
 from django.utils import timezone
+from time import sleep
+from random import randint
 
 from core.models import (
     AssetData,
@@ -9,18 +12,23 @@ from core.scrapers.coindesk import CoindeskScraper
 
 @shared_task(name="scrape_to_coindesk")
 def scrape_to_coindesk(scraper_id) -> None:
+    try:
+        scraper: Scraper = Scraper.objects.get(id=scraper_id)
+        coindesk: CoindeskScraper = CoindeskScraper(url=scraper.url)
 
-    scraper: Scraper = Scraper.objects.get(id=scraper_id)
-    coindesk: CoindeskScraper = CoindeskScraper(url=scraper.url)
+        data_scraper: dict = coindesk.get_detail()
 
-    data_scraper: dict = coindesk.get_detail()
+        asset_data: AssetData = AssetData(**data_scraper)
+        asset_data.asset = scraper.asset
+        asset_data.data_datetime = timezone.now()
 
-    asset_data: AssetData = AssetData(**data_scraper)
-    asset_data.asset = scraper.asset
-    asset_data.data_datetime = timezone.now()
-    asset_data.save()
+        asset_data.save()
 
-    print(f'Add data to {scraper.asset.name}')
-    del coindesk
-    del data_scraper
-    del asset_data
+        print(f'Add data to {scraper.asset.name}')
+        del coindesk
+        del data_scraper
+        del asset_data
+
+    except Exception as e:
+        print(e)
+        print(f'Problem with scrape {scraper_id}')
